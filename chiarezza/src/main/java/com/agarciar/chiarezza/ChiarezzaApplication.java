b5b41527ad9dd9d591c1bccdbfe1962a56c17f0f
Andres <andresgr2094@hotmail.com>
2023-08-03 00:03:10 -0400

    initial gradle commit

---
+++ chiarezza/src/main/java/com/agarciar/chiarezza/ChiarezzaApplication.java
@@ -0,0 +1,13 @@
+package com.agarciar.chiarezza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiarezzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiarezzaApplication.class, args);
+	}
+
+}
---
+++ chiarezza/src/test/java/com/agarciar/chiarezza/ChiarezzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agarciar.chiarezza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiarezzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
